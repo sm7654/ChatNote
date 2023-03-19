62f287ee042bd501414d748b4241ce114c2a6c29
sm7654 <115954704+sm7654@users.noreply.github.com>
2023-03-19 21:25:54 +0200

    Add files via upload

---
+++ ChatnoteJavaScript.js
@@ -0,0 +1,46 @@
+function bars_clicked(){ 
+    var bars = document.getElementById("heello");
+    var bar1 = document.getElementById("bar1");
+    var bar2 = document.getElementById("bar2");
+    var bar3 = document.getElementById("bar3");
+    if (bars.style.display == "none") {
+        bars.style.display = "block";
+        bar1.style.rotate = "-45deg";
+        bar1.style.position = "absolute";
+        bar2.style.opacity ="0";
+        bar3.style.position = "absolute";
+        bar3.style.rotate = "45deg";
+    } else {
+        bars.style.display = "none";
+        bar1.style.rotate = "0deg";
+        bar1.style.position = "relative";
+        bar2.style.opacity ="1";
+        bar3.style.position = "relative";
+        bar3.style.rotate = "0deg";
+    }
+}
+document.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      send();
+    }
+  });
+
+function send() {
+    var chat_wraper = document.getElementById("chat_wraper");
+    var inputvalue = document.getElementById("input").value;
+    
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes().toString().padStart(2, "0");
+    var time = hours + ":" + minutes;
+    var message = `<div class="messege_conteiner"><div class="message_box"><p style="color: #edededed; font-size: 12px;">Anonymous</p><p>${inputvalue}</p><span style="font-size: 11px; color: #edededed;">${time}</span></div><img src="./images/user (1).png" alt="" class="avatar"></div>`;
+    if (inputvalue !== '') {
+        chat_wraper.insertAdjacentHTML('beforeend', message);
+        document.getElementById("input").value = '';
+    } else {
+        return;
+    }
+    chat_wraper.scrollTop = chat_wraper.scrollHeight;
+    console.log();(scrollHeight)
+}
