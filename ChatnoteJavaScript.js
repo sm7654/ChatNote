f76a55d53a336cbd5af41da256bd7147bd0a8a7e
sm7654 <115954704+sm7654@users.noreply.github.com>
2023-03-25 03:33:23 +0300

    Add files via upload

--- ChatnoteJavaScript.js
+++ ChatnoteJavaScript.js
@@ -1,6 +1,5 @@
 var UserName = "Anonymous";
-var BackgroundLinkLight = "url(./images/backgrounds/cool-background3.png)";
-var BackgroundLinkDark = "url(./images/backgrounds/background.jpg)";
+
 function bars_clicked() {
     document.getElementById("you_account_settings").innerHTML = UserName;
     var x = document.getElementById("settings_wraper");
@@ -22,6 +21,8 @@ function bars_clicked() {
         bar3.style.position = "relative";
         bar3.style.rotate = "0deg";
         x.style.width = "0px";
+        document.getElementById("drop_down_icon").style.rotate = "0deg";
+        document.getElementById("ul_background_options").style.opacity = "0";
     }
 }
 document.addEventListener("keypress", function(event) {
@@ -57,7 +58,6 @@ function dark_theme() {
     var input = document.getElementById("input");
     var header = document.getElementById("header");
     var bars = document.getElementsByClassName("bars");
-    var background = document.getElementsByClassName("background");
     var setting = document.getElementsByClassName("settings_wraper");
     var account_buttons = document.getElementsByClassName("account");
     var text_settings = document.getElementsByClassName("setting_text");
@@ -72,14 +72,10 @@ function dark_theme() {
     for (let i = 0; i < 2; i++) {
         account_buttons[i].style.background = "#1B1B1B";
         account_buttons[i].style.border = "1px solid #1B1B1B";
-        document.getElementsByClassName("dark_light_options")[i].style.background = "#222222";
-        document.getElementsByClassName("dark_light_options")[i].style.color = "#ffffff";
     }
     input_wraper[0].style.background = "#0C0404";
     input.style.background = "#1B1B1B";
     input.style.color = "white";
-    background[0].style.background = `${BackgroundLinkDark}`;
-    background[0].style.backgroundSize = "cover";
     setting[0].style.background = "black";
     for (let i = 0; i < 3; i++) {
         icons_settings[i].style.fill = "white";
@@ -88,7 +84,6 @@ function dark_theme() {
         document.getElementsByClassName("heading_color_theme")[0].style.borderBottom = "1px solid #FFA500";
     }
     document.getElementById("drop_down_icon").style.fill = "#FFA500";
-    document.getElementById("dark_light_background_option_wraper").style.background = "#333333";
 }
 
 function light_theme() {
@@ -96,7 +91,6 @@ function light_theme() {
     var input = document.getElementById("input");
     var header = document.getElementById("header");
     var bars = document.getElementsByClassName("bars");
-    var background = document.getElementsByClassName("background");
     var setting = document.getElementsByClassName("settings_wraper");
     var account_buttons = document.getElementsByClassName("account");
     var text_settings = document.getElementsByClassName("setting_text");
@@ -111,79 +105,33 @@ function light_theme() {
     for (let i = 0; i < 2; i++) {
         account_buttons[i].style.background = "black";
         account_buttons[i].style.border = "1px solid black";
-        document.getElementsByClassName("dark_light_options")[i].style.background = "whitesmoke";
-        document.getElementsByClassName("dark_light_options")[i].style.color = "black";
     }
     input_wraper[0].style.background = "#ededed";
     input.style.background = "white";
     input.style.color = "black";
-    background[0].style.background = `${BackgroundLinkLight}`;
-    background[0].style.backgroundSize = "cover";
     setting[0].style.background = "white";
     for (let i = 0; i < 3; i++) {
-        icons_settings[i].style.fill = "black";
-        text_settings[i].style.color = "black";
+        icons_settings[i].style.fill = "";
+        text_settings[i].style.color = "";
         document.getElementsByClassName("heading_color_theme")[0].style.color = "black";
         document.getElementsByClassName("heading_color_theme")[0].style.borderBottom = "1px solid #ededed";
     }
     document.getElementById("drop_down_icon").style.fill = "black";
-    document.getElementById("dark_light_background_option_wraper").style.background = "whitesmoke";
 }
 function background_option_clicked() {
-    for (let i = 0; i < 2; i++) 
-    {
-        if (document.getElementsByClassName("dark_light_options")[i].style.height === "0px") {
-            document.getElementsByClassName("dark_light_options")[i].style.height = "auto";
-            document.getElementsByClassName("dark_light_options")[i].style.padding = "5px 15px";
-            document.getElementById("drop_down_icon").style.rotate = "1620deg";
-        } else {
-            document.getElementsByClassName("dark_light_options")[i].style.height = "0px";
-            document.getElementsByClassName("dark_light_options")[i].style.padding = "0px 0px";
-            document.getElementById("drop_down_icon").style.rotate = "0deg";
-        }
+    var x = document.getElementById("ul_background_options")
+    if (x.style.opacity === "0") {
+        x.style.opacity = "1";
+        x.style.pointerEvents = "all";
+        document.getElementById("drop_down_icon").style.rotate = "1620deg";
+    } else {
+        x.style.opacity = "0";
+        x.style.pointerEvents = "none";
+        document.getElementById("drop_down_icon").style.rotate = "0deg";
     }
 }
-function Dark_Mode_background() {
-    var dark = (document.getElementById("dark_option_background"));
-    var light = (document.getElementById("light_option_background"));
-    var light_text = (document.getElementById("light_text_background"))
-    if (dark.style.width === "100%") {
-        light.style.opacity = "1";
-        light.style.pointerEvents = "all";
-        light.style.padding = "5px 15px";
-        light.style.flexGrow = "1";
-        light_text.style.width = "auto";
-        light_text.style.height = "auto";
-        dark.style.width = "";
-    } else {
-        light.style.opacity = "0";
-        light.style.pointerEvents = "none";
-        light.style.padding = "0px 0px";
-        light.style.flexGrow = "0";
-        light_text.style.width = "0px";
-        light_text.style.height = "0px";
-        dark.style.width = "100%";
-    }
-}
-function Light_Mode_background() {
-    var dark = (document.getElementById("dark_option_background"));
-    var light = (document.getElementById("light_option_background"));
-    var dark_text = (document.getElementById("dark_text_background"))
-    if (light.style.width === "100%") {
-        dark.style.opacity = "1";
-        dark.style.pointerEvents = "all";
-        dark.style.padding = "5px 15px";
-        dark.style.flexGrow = "1";
-        dark_text.style.width = "auto";
-        dark_text.style.height = "auto";
-        light.style.width = "";
-    } else {
-        dark.style.opacity = "0";
-        dark.style.pointerEvents = "none";
-        dark.style.padding = "0px 0px"
-        dark.style.flexGrow = "0";
-        dark_text.style.width = "0px";
-        dark_text.style.height = "0px";
-        light.style.width = "100%";
-    }
+function background_update(name) {
+    var url = document.getElementsByClassName("background");
+    url[0].style.background = name;
+
 }
